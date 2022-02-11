a7aa730ad6b157e5dfc2aba9e03f39450e7df264
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-02-11 20:37:33 -0300

    all the ease of jpa

--- src/main/java/application/ProgramJpa.java
+++ src/main/java/application/ProgramJpa.java
@@ -10,18 +10,17 @@ public class ProgramJpa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana MAria", "ana@gmail.com");
+
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();		
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+
         System.out.println("Ready!");
+        em.close();
+        emf.close();
 
 	}
 }
